a6e76b44d193ea6ee3c7205832bd14302772313b
Yukohana <jpsky@outlook.jp>
2025-05-04 18:15:29 +0900

    kadai

--- src/kadai_018/KatoHanako_Chapter18.java
+++ src/kadai_018/KatoHanako_Chapter18.java
@@ -2,17 +2,13 @@ package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18 {
 
-	
     public KatoHanako_Chapter18() {
-        setGivenName("花子");
+        this.familyName = "加藤";
+        this.givenName = "花子";
+        this.address = "東京都中野区〇×";
     }
 
-    private void setGivenName(String string) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
-	@Override
+    @Override
     public void eachIntroduce() {
         System.out.println("趣味は読書です");
     }
--- src/kadai_018/KatoIchiro_Chapter18.java
+++ src/kadai_018/KatoIchiro_Chapter18.java
@@ -2,17 +2,13 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 
-	
     public KatoIchiro_Chapter18() {
-        setGivenName("一郎");
+        this.familyName = "加藤";
+        this.givenName = "一郎";
+        this.address = "東京都中野区〇×";
     }
 
-    private void setGivenName(String string) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
-
-	@Override
+    @Override
     public void eachIntroduce() {
         System.out.println("好きな食べ物はリンゴです");
     }
